bac8ada97e408ef39f3233c4500c62f31a231759
qz <zhaoqige@163.com>
2017-07-25 16:11:36 +0800

    Import HMACSha() from "Azure IoTHub SASToken" project

---
+++ c-openssl/c-hma-hash/zhash.h
@@ -0,0 +1,16 @@
+/*
+ * zhash.h
+ *
+ *  Created on: Jul 25, 2017
+ *      Author: Qige <qigezhao@gmail.com>
+ */
+
+#ifndef ZHASH_H_
+#define ZHASH_H_
+
+int zHashHMAC(const char *algo,
+		const char *key, unsigned int keyLength,
+		char *input, unsigned int inputLength,
+		char *output, unsigned int *outputLength);
+
+#endif /* ZHASH_H_ */
