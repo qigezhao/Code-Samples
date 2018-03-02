6d17851cc9dbceeb81bc06b9dfd79fbeb5eb74e4
qz <qigezhao@gmail.com>
2018-03-02 17:33:18 +0800

    Python: 2017 city pouplations (practise)
    - parse string to .csv;
    - find which province the city belongs to;
    - manually fix blanks;
    - manually convert to .xlsx

---
+++ py-examples/city_populations/01_city_cols.py
@@ -0,0 +1,36 @@
+﻿#!/usr/bin/python
+# charset: utf-8
+# by Qige <qigezhao@gmail.com> since 2018.03.02
+
+import re
+
+
+# string.split(',')
+def splitCityRawLine(idxNamePop):
+    if idxNamePop:
+        l1 = idxNamePop.strip()
+        l2 = re.sub('\u3000','',l1)
+        idx, pop = re.findall('[0-9\.]+', l2)
+        name = re.search('[^0-9\.]+', l2).group()
+        return idx, name, pop
+
+    return None, None, None
+
+
+# split into index,city,populations
+def main(fin = 'city_raw.txt', fout = 'city.csv'):
+    fd = open(fin,'r', encoding = 'utf-8')
+    fd2 = open(fout, 'w', encoding = 'utf-8')
+    if fd and fd2:
+        for line in fd.readlines():
+            idx, name, pop = splitCityRawLine(line)
+            if idx:
+                fd2.write("%s,%s,%s\n" %(idx, name, pop))
+
+        fd.close()
+        fd2.close()
+
+
+# cli call
+if __name__=="__main__":
+    main()
---
+++ py-examples/city_populations/02_city_prov.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# charset: utf-8
+# by Qige <qigezhao@gmail.com> since 2018.03.02
+
+import re
+
+import requests
+#from bs4 import BeautifulSoap
+from bs4 import BeautifulSoup
+
+
